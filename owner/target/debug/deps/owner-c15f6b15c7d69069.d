7bd41030e2b928b841a7ac5bdd1f4d4946c04abf
ubervelocity <mihaitu.popescu@gmail.com>
2019-05-13 18:53:36 +0200

    added ownership chapter

---
+++ owner/target/debug/deps/owner-c15f6b15c7d69069.d
@@ -0,0 +1,5 @@
+/home/merkel/Documents/Personal/rust/gitrust/rust-book/owner/target/debug/deps/owner-c15f6b15c7d69069: src/main.rs
+
+/home/merkel/Documents/Personal/rust/gitrust/rust-book/owner/target/debug/deps/owner-c15f6b15c7d69069.d: src/main.rs
+
+src/main.rs:
