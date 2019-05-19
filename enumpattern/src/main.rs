8db9825a059409a96e5aeebe3532c27c3ddf447b
ubervelocity <mihaitu.popescu@gmail.com>
2019-05-19 23:44:46 +0200

    Option<T> and Match

--- enumpattern/src/main.rs
+++ enumpattern/src/main.rs
@@ -3,6 +3,13 @@ enum IpAddrKind {
     V6(String),
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32},
@@ -33,6 +40,18 @@ fn route(ip_type: IpAddrKind) {
 
 }
 
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => {
+            println!("oooo a quarter!");
+            25
+        }
+    }
+}
+
 fn main() {
     let home = IpAddrKind::V4(127, 0, 0, 1);
 
@@ -52,4 +71,31 @@ fn main() {
 
     m.call();
 
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);  // Value can be empty with Option.
+    assert_eq!(y.is_some(), true);
+
+    // let sum = x + y; // Conversion from Option<T> to T required
+    // before being able to sum.
+
+    // Not having to worry about incorrectly assuming a not-null value
+    // helps you to be more confident in your code. In order to have a 
+    // value that can possibly be null, you must explicitly opt in by 
+    // making the type of that value Option<T>. Then, when you use that 
+    // value, you are required to explicitly handle the case when the value 
+    // is null. Everywhere that a value has a type that isn’t an Option<T>, you 
+    // can safely assume that the value isn’t null. This was a deliberate design 
+    //decision for Rust to limit null’s pervasiveness and increase the safety of Rust 
+    //code.
+
+    // -------- MATCH --------
+    let nick = Coin::Nickel;
+    println!("A nickel is {} cents!", value_in_cents(nick));
+    let quarter = Coin::Quarter;
+    println!("{}", value_in_cents(quarter));
 }
