e197d57715985a59eab5494eb6e405ecc94f5675
ubervelocity <mihaitu.popescu@gmail.com>
2019-08-06 08:46:14 +0200

    lifetimes

---
+++ lifetimes/src/lifetime.rs
@@ -0,0 +1,47 @@
+// pub fn run() {
+//     let r;
+
+//     {
+//         let x = 5;
+//         r = &x;
+//     }
+//     // 'x' scope ends at the block, so 'r' is a dangling reference
+
+//     println!("r: {}", r);
+// }
+
+// We don't know which borrowed reference will be returned (x or y).
+// So the output parameter requires a generic lifetime parameter. 
+// pub fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } 
+//     else {
+//         y
+//     }
+// }
+
+// Specifying lifetime parameters:
+// &i32             reference to an i32
+// &'a i32          reference to an i32 with an explicit lifetime
+// &'a mut i32      mutable reference to an i32 with an explicit lifetime 
+
+// If two variables have the lifetime 'a, it means that both of them must live 
+// as long as the generic lifetime 'a.
+
+pub fn longest2<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    }
+    else {
+        y
+    }
+}
+
+pub fn run2() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest2(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
